eabfd719865e4fa8609824b0ed6c9bed2f6eb3f9
Maria Soria <maria.it3@gmail.com>
2023-03-23 12:50:36 +0000

    Refactor tests

--- src/test/kotlin/dev/kata/templateengine/TemplateEngineShould.kt
+++ src/test/kotlin/dev/kata/templateengine/TemplateEngineShould.kt
@@ -8,16 +8,15 @@ internal class TemplateEngineShould {
     fun `TemplateEngine should not replace anything if there is not a variable in the text`() {
         val textWithoutVariables = "This is just a text without anything to replace"
         val replacement = mapOf("variable" to "irrelevant")
-        assertEquals("This is just a text without anything to replace",
-            TemplateEngine.replace(textWithoutVariables, replacement))
+        assertEquals(textWithoutVariables, TemplateEngine.replace(textWithoutVariables, replacement))
     }
 
     @Test
     fun `TemplateEngine should replace the expression with a blank if the variable is not present in the text`() {
         val textWithANonExistingVariable = "This is a text with a \${differentVariable} to be replaced"
         val replacement = mapOf("non-existing-variable" to "irrelevant")
-        assertEquals("This is a text with a   to be replaced",
-            TemplateEngine.replace(textWithANonExistingVariable, replacement))
+        val expectedResult = "This is a text with a   to be replaced"
+        assertEquals(expectedResult, TemplateEngine.replace(textWithANonExistingVariable, replacement))
     }
 
     @Test
@@ -25,8 +24,8 @@ internal class TemplateEngineShould {
         val text = "This is a text with a \${variable} to be replaced. " +
                 "And this is another text with the same \${variable} to be replaced."
         val replacement = mapOf("variable" to "variable")
-        assertEquals("This is a text with a variable to be replaced. " +
-                "And this is another text with the same variable to be replaced.",
-            TemplateEngine.replace(text, replacement))
+        val expectedResult = "This is a text with a variable to be replaced. " +
+                "And this is another text with the same variable to be replaced."
+        assertEquals(expectedResult, TemplateEngine.replace(text, replacement))
     }
 }
