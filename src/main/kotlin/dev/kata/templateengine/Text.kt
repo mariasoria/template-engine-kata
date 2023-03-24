488abc2631e9d5dd66a526c1ba353d210abab994
Maria Soria <maria.it3@gmail.com>
2023-03-24 13:49:20 +0000

    Refactor Text Object constructor

--- src/main/kotlin/dev/kata/templateengine/Text.kt
+++ src/main/kotlin/dev/kata/templateengine/Text.kt
@@ -1,14 +1,12 @@
 package dev.kata.templateengine
 
-class Text {
+class Text(private var text: String, replacement: Map<String, String>) {
 
-    private var text: String
     private val variableName: String
     private val variableValue: String
     private val expression: String
 
-    constructor(text: String, replacement: Map<String, String>) {
-        this.text = text
+    init {
         this.variableName = replacement.keys.first()
         this.variableValue = replacement[variableName].toString()
         this.expression = "\${$variableName}"
